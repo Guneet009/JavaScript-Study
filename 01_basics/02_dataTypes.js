2e3c00aa351674d2f84a7b08c7bb3dbf75512dd4
Guneet009 <guneet1308@gmail.com>
2025-10-16 15:23:21 +0530

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" 
+/* 
+By writing this we are saying that we are using new version of JavaScript
+It is no longer necessary to write
+*/
+
+let name = "Jhon";  // String
+let age = 25; // number
+let isLoggedIn = true; // Boolean
+let state; // undefined
+let balance = 2300.50; // number
+let dateOfBirth = null; //object
+
+/*
+Datatypes in Java Script
+ - String - Can be represented by double or single quotes
+ - Number - Numeric value
+ - bigint - Large numeric value
+ - boolean - true/false
+ - null - standalone value/object
+ - undefined - undefined
+ - symbol - unique
+ - object
+ */
+
+console.table([typeof(name),typeof(age),typeof(isLoggedIn),typeof(state),typeof(balance),typeof(dateOfBirth)]);
